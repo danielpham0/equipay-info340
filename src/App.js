6b3fece1360a893036948145c62026ecdcb705ab
Daniel Pham <danielpham0@gmail.com>
2021-05-24 17:41:42 -0700

    basic react app outline

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
-function App() {
+// import React, { useState } from 'react';
+// import pages from other js files
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Route, Switch, Link, Redirect, NavLink } from 'react-router-dom'
+import GOOGLE_EMPLOYEES from './google.csv'
+// most likely need to change this since react doesn't read csv the same as json
+
+function App(props) {
+  const employees = GOOGLE_EMPLOYEES;
+
   return (
     <div className="App">
+      <header>
+        <h1><Link to='/landing'>EquiPay</Link></h1>
+        <div class="creators">
+          <p>Daniel Pham, Shane Fretwel, Ryan Carroll</p>
+        </div>
+      </header>
+      <main>
+        <Nav />
+        <div>
+          <Switch>
+            <Route path='/landing'> </Route>
+            <Route exact path='/'> </Route>
+            <Route path='/roles/:company'> </Route>
+            <Route path='/chart/:company/:role'> </Route>
+            <Route path='/form'> </Route>
+            <Redirect to='/' />
+          </Switch>
+        </div>
+      </main>
+      <footer class="footer">
+        <p>Daniel Pham, Shane Fretwel, Ryan Carroll</p>
+        <p>INFO 340 Front End Development</p>
+        <p><a href="mailto:ischool@uw.edu">ischool@uw.edu</a></p>
+      </footer>
     </div>
   );
+}
+
+function Nav(){
+  // Most likely will have to take in props from url to display the nav properly
+  return(<div>
+    <nav>
+      <ul>
+        <li><NavLink exact to="/" activeClassName="active"> Companies </NavLink></li>
+        <li> {'>'} </li>
+        <li><NavLink exact to="/roles/:company" activeClassName="active"> Google </NavLink></li>
+      </ul>
+    </nav>
+  </div>);
 }
 
 export default App;
