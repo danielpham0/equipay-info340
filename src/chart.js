73cdb027cf293bdb4b7267ff949a2340e8987f39
fretws <fretws@uw.edu>
2021-05-26 16:12:42 -0700

    Add script for starting development server

--- src/chart.js
+++ src/chart.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
+
 import {CompanyHeader} from './App';
 
 function ChartPage() {
@@ -20,7 +22,19 @@ function ChartPage() {
 
 function Chart() {
   return (
-    <div id="chart" className="mx-auto"></div>
+    <Plot
+      data={[
+        {
+          x: [1, 2, 3],
+          y: [2, 6, 3],
+          type: 'scatter',
+          mode: 'lines+markers',
+          marker: {color: 'red'},
+        },
+        {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+      ]}
+      layout={{width: 320, height: 240, title: 'A Fancy Plot'}}
+    />
   );
 }
 
