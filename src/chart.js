b140ab7efdcb32111f1ff5e43ed420aa25736207
Daniel Pham <danielpham0@gmail.com>
2021-05-26 13:07:02 -0700

    Input data & static role cards/list

--- src/chart.js
+++ src/chart.js
@@ -7,10 +7,10 @@ function ChartPage() {
       <h2 className="pt-4 px-4">Compare against the baseline</h2>
       <CompanyHeader company='Google' />
       <div className='container py-3'>
-        <div class="col-12 col-lg-3">
+        <div className="col-12 col-lg-3">
           <ChartForm />
         </div>
-        <div class="col-12 col-lg-9">
+        <div className="col-12 col-lg-9">
           <Chart />
         </div>
       </div>
@@ -20,41 +20,41 @@ function ChartPage() {
 
 function Chart() {
   return (
-    <div id="chart" class="mx-auto"></div>
+    <div id="chart" className="mx-auto"></div>
   );
 }
 
 function ChartForm() {
   return (
-    <form id="chartForm" class="form">
-      <div class="form-group row">
-        <label for="genderInput" class="col-lg-1">Gender</label>
-        <div class="col-lg-11">
-          <select id="genderInput" name="gender" class="form-control" required>
+    <form id="chartForm" className="form">
+      <div className="form-group row">
+        <label htmlFor="genderInput" className="col-lg-1">Gender</label>
+        <div className="col-lg-11">
+          <select id="genderInput" name="gender" className="form-control" required>
             <option value="All">All</option>
           </select>
         </div>
       </div>
-      <div class="form-group row">
-        <label for="ethnicityInput" class="col-lg-1">Ethnicity</label>
-        <div class="col-lg-11">
-          <select id="ethnicityInput" name="ethnicity" class="form-control" required>
+      <div className="form-group row">
+        <label htmlor="ethnicityInput" className="col-lg-1">Ethnicity</label>
+        <div className="col-lg-11">
+          <select id="ethnicityInput" name="ethnicity" className="form-control" required>
             <option value="All">All</option>
           </select>
         </div>
       </div>
-      <div class="form-group row">
-        <label for="orientationInput" class="col-lg-1">Sexual Orientation</label>
-        <div class="col-lg-11">
-          <select id="orientationInput" name="sexual orientation" class="form-control" required>
+      <div className="form-group row">
+        <label htmlFor="orientationInput" className="col-lg-1">Sexual Orientation</label>
+        <div className="col-lg-11">
+          <select id="orientationInput" name="sexual orientation" className="form-control" required>
             <option value="All">All</option>
           </select>
         </div>
       </div>
-      <div class="row">
-        <div class="col col-lg-11 d-flex flex-column">
-          <button id="change-chart" type="submit" class="btn btn-primary d-block my-3">Change Chart</button>
-          <p id="chart-result" class="alert d-none"></p>
+      <div className="row">
+        <div className="col col-lg-11 d-flex flex-column">
+          <button id="change-chart" type="submit" className="btn btn-primary d-block my-3">Change Chart</button>
+          <p id="chart-result" className="alert d-none"></p>
         </div>
       </div>
     </form>
