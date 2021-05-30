3c133d788e3b2b710e0e6bad2426c18cc729dfb3
Shane Fretwell <fretws@uw.edu>
2021-05-29 18:32:20 -0700

    Make chart form reflect filters in URL and chart, also add chart form and page files that were not added in last commit

---
+++ src/ChartPage.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {useParams} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import {CompanyHeader, Nav} from './App';
+import Chart from './Chart';
+import ChartForm from './ChartForm';
+
+function ChartPage(props) {
+  const urlParams = useParams();
+  // capitalizing first letter of each using method here:
+  // https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/
+  let role = urlParams.role.replace(/_/g, ' ');
+  role = role.split(' ').map((str => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  })).join(' ');
+  let company = urlParams.company;
+  let navLinks = [{Companies: "/"},
+    {[company.charAt(0).toUpperCase() + company.slice(1)]: "/roles/" + company},
+    {[role]: "/chart/" + company + "/" + urlParams.role}];
+
+  const [chartNeedsUpdate, setUpdate] = React.useState(false);
+  return (
+    <>
+      <Nav links={navLinks} />
+      <h2 className="pt-4 px-4">Compare against the baseline</h2>
+      <CompanyHeader company={urlParams.company} description={role} />
+      <Container fluid>
+        <Row>
+          <Col lg={3}>
+            <ChartForm data={props.data} setUpdate={setUpdate} />
+          </Col>
+          <Col lg={9}>
+            <Chart data={props.data} chartNeedsUpdate={chartNeedsUpdate} setUpdate={setUpdate} />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default ChartPage;
