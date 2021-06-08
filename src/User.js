35201b7efc00a5f1cbee7adee7cb865d0868d611
Daniel Pham <danielpham0@gmail.com>
2021-06-08 14:29:58 -0700

    test

--- src/User.js
+++ src/User.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 import {Nav} from './App';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 
 function UserPage(props) {
+    const [user, setUser] = useState(undefined);
+    let content = null;
+    if(!user){
+
+    }
     return (
       <div>
         <Nav links={[{'Home': '/'}]}/>
